c7df8ea0b34dfa3ef789bfb37d9831b0b56d84df
Purrygamer <bperez220@hotmail.com>
2024-01-01 21:32:02 -0500

    Recreated file that was eated

---
+++ Generator/mod_generator.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define COMPILE_COMMAND "gcc -S -masm=intel "
+#define MAX_PATH 256
+
+const char* header_code[14] = {
+    "\tnop rcx\n\t", "push rax\n\t", "push rbx\n\t", "push rcx\n\t", "push rdx\n\t", 
+	"push rsi\n\t", "push rdi\n\t", "push r8\n\t", "push r9\n\t", "push r10\n\t", 
+    "push r11\n\t", "push r12\n\t", "push r13\n\t", "push r14\n",
+};
+
+const char* ending_code[25] = {
+    "pop r15\n\t", "pop r14\n\t", "pop r13\n\t", "pop r12\n\t", 
+	"pop r11\n\t", "pop r10\n\t", "pop r9\n\t", "pop r8\n\t", 
+	"pop rdi\n\t", "pop rsi\n\t", "pop rdx\n\t", "pop rcx\n\t", 
+	"pop rbx\n\t", "pop rax\n\t", "mov temp_rax[rip + 0], rax\n\t", 
+	"pop rax\n\t", "mov temp_ret[rip + 0], rax\n\t", "mov rax, temp_rax[rip + 0]\n\t", 
+	"nop rax\n\t", "call   0xffffffffffabcdef\n\t", "mov temp_rax[rip + 0], rax\n\t", 
+	"mov rax, temp_ret[rip + 0]\n\t", "push rax\n\t", "mov rax, temp_rax[rip + 0]\n\t", 
+	"ret\n\t", 
+};
+
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        printf("Supply a path to a file (ex. path\\to\\file.c)\n");
+        return -1;
+    }
+    if(strlen(argv[1]) > MAX_PATH){
+        printf("File path/name too long\n");
+    }
+    printf("Compiling %s\n", argv[1]);
+    char compile_command[MAX_PATH + sizeof(COMPILE_COMMAND)];
+    strcpy(compile_command, COMPILE_COMMAND);
+    strcat(compile_command, argv[1]);
+    printf("issuing %s\n", compile_command);
+    system(compile_command);
+    //Now insert needed code into assembler source
+    printf("Inserting header/ender code\n");
+    FILE* original_source;
+    FILE* gen_source;
+    char file_name[MAX_PATH];
+    strcpy(file_name, argv[1]);
+    file_name[strlen(file_name) - 1] = 's';
+    printf("Assembler source: %s\n", file_name);
+    original_source = fopen(file_name, "r");
+    gen_source = fopen("dkcedit_out.s", "w+");
+    if(original_source == NULL) {
+        printf("Invalid path for the file\n");
+    }
+    char file_buffer[MAX_PATH];
+    while(fgets(file_buffer, MAX_PATH, original_source) != NULL) {
+        if(strcmp(file_buffer, "mod_main:\n") == 0){
+            fwrite(file_buffer, strlen(file_buffer), 1, gen_source);
+            printf("mod_main found\n");
+            for(int i = 0; i < 14; i++){
+                fwrite(header_code[i], strlen(header_code[i]), 1, gen_source);
+            }
+        }
+        else if(strcmp(file_buffer, "\t.seh_endproc\n") == 0) {
+            printf("End of code found\n");
+            fseek(gen_source, -5, SEEK_CUR); //moves back to the start of the ret\n line
+            for(int i = 0; i < 25; i++){
+                fwrite(ending_code[i], strlen(ending_code[i]), 1, gen_source);
+            }
+            fwrite(file_buffer, strlen(file_buffer), 1, gen_source);
+        }
+        else {
+            fwrite(file_buffer, strlen(file_buffer), 1, gen_source);
+        }
+    }
+    fclose(original_source);
+    fclose(gen_source);
+    printf("Assembling the code\n");
+    system("as -o dkcedit_out.o dkcedit_out.s");
+    printf("Dumping text segment\n");
+    system("objcopy -O binary --only-section=.text dkcedit_out.o mod.bin");
+    printf("mod.bin created.\nIf there is no mod.bin then there was a compilation error.\n");
+    return 0;
+}
---
+++ Generator/test.s
@@ -0,0 +1,122 @@
+	.file	"test.c"
+	.intel_syntax noprefix
+	.text
+	.globl	new_attack
+	.section	.text,"w"
+	.align 8
+new_attack:
+	.ascii "Attack(####)\0"
+	.globl	temp_rax
+	.align 8
+temp_rax:
+	.ascii "1234567\0"
+	.globl	temp_ret
+	.align 8
+temp_ret:
+	.ascii "ABCDEFG\0"
+	.text
+	.globl	mod_main
+	.def	mod_main;	.scl	2;	.type	32;	.endef
+	.seh_proc	mod_main
+mod_main:
+	push	rbp
+	.seh_pushreg	rbp
+	mov	rbp, rsp
+	.seh_setframe	rbp, 0
+	sub	rsp, 16
+	.seh_stackalloc	16
+	.seh_endprologue
+	mov	DWORD PTR -4[rbp], 0
+	mov	eax, DWORD PTR -4[rbp]
+/APP
+ # 6 "C:\Users\perezbe\Documents\GitHub\DKCedit\Generator\test.c" 1
+	.intel_syntax noprefix
+	nop %rbx
+	mov eax, [%rip-0x2dc997]
+	mov eax, [eax+0x1d04]
+ # 0 "" 2
+/NO_APP
+	mov	DWORD PTR -4[rbp], eax
+	mov	ecx, DWORD PTR -4[rbp]
+	mov	edx, 274877907
+	mov	eax, ecx
+	imul	edx
+	sar	edx, 6
+	mov	eax, ecx
+	sar	eax, 31
+	sub	edx, eax
+	mov	eax, edx
+	add	eax, 48
+	mov	BYTE PTR new_attack[rip+7], al
+	mov	ecx, DWORD PTR -4[rbp]
+	mov	edx, 274877907
+	mov	eax, ecx
+	imul	edx
+	sar	edx, 6
+	mov	eax, ecx
+	sar	eax, 31
+	sub	edx, eax
+	mov	eax, edx
+	imul	eax, eax, 1000
+	sub	ecx, eax
+	mov	eax, ecx
+	mov	DWORD PTR -4[rbp], eax
+	mov	ecx, DWORD PTR -4[rbp]
+	mov	edx, 1374389535
+	mov	eax, ecx
+	imul	edx
+	sar	edx, 5
+	mov	eax, ecx
+	sar	eax, 31
+	sub	edx, eax
+	mov	eax, edx
+	add	eax, 48
+	mov	BYTE PTR new_attack[rip+8], al
+	mov	ecx, DWORD PTR -4[rbp]
+	mov	edx, 1374389535
+	mov	eax, ecx
+	imul	edx
+	sar	edx, 5
+	mov	eax, ecx
+	sar	eax, 31
+	sub	edx, eax
+	mov	eax, edx
+	imul	eax, eax, 100
+	sub	ecx, eax
+	mov	eax, ecx
+	mov	DWORD PTR -4[rbp], eax
+	mov	ecx, DWORD PTR -4[rbp]
+	mov	edx, 1717986919
+	mov	eax, ecx
+	imul	edx
+	sar	edx, 2
+	mov	eax, ecx
+	sar	eax, 31
+	sub	edx, eax
+	mov	eax, edx
+	add	eax, 48
+	mov	BYTE PTR new_attack[rip+9], al
+	mov	ecx, DWORD PTR -4[rbp]
+	mov	edx, 1717986919
+	mov	eax, ecx
+	imul	edx
+	sar	edx, 2
+	mov	eax, ecx
+	sar	eax, 31
+	sub	edx, eax
+	mov	eax, edx
+	sal	eax, 2
+	add	eax, edx
+	add	eax, eax
+	sub	ecx, eax
+	mov	eax, ecx
+	mov	DWORD PTR -4[rbp], eax
+	mov	eax, DWORD PTR -4[rbp]
+	add	eax, 48
+	mov	BYTE PTR new_attack[rip+10], al
+	nop
+	add	rsp, 16
+	pop	rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-sjlj-rev0, Built by MinGW-W64 project) 8.1.0"
